39d9648269b670126330be35e5eb60fd575560a0
Ynikum <alexfalatyuk98@gmail.com>
2022-02-14 16:04:08 +0200

    Created first fragment and set navigation with progress bar

--- app/src/main/java/com/example/applicationforgravity/MainActivity.kt
+++ app/src/main/java/com/example/applicationforgravity/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.applicationforgravity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.applicationforgravity.loading.FragmentWithLoading
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null){
+            toFragmentWithLoading()
+        }
     }
+
+
+
+    private fun toFragmentWithLoading(){
+        supportFragmentManager.beginTransaction()
+            .add(
+                R.id.flMain,
+                FragmentWithLoading.create(),
+                FragmentWithLoading::class.java.simpleName
+            )
+            .addToBackStack("trans:${FragmentWithLoading::class.java.simpleName}")
+            .commit()
+    }
+
 }
--- app/src/main/java/com/example/applicationforgravity/loading/FragmentWithLoading.kt
+++ app/src/main/java/com/example/applicationforgravity/loading/FragmentWithLoading.kt
@@ -1,4 +1,30 @@
 package com.example.applicationforgravity.loading
 
-class FragmentWithLoading {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.applicationforgravity.R
+
+class FragmentWithLoading: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_loading_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+
+
+
+    companion object{
+        fun create() = FragmentWithLoading()
+    }
 }
